191d0a07200d0d3c61f1c20ceaf51a8bb5cd96fc
baliram123giri <giri710401@gmail.com>
2023-11-23 15:28:40 +0530

    ssl test 14

--- Utils/auth.utils.js
+++ Utils/auth.utils.js
@@ -17,7 +17,14 @@ function verifyAccessToken(token) {
 
 // Set a cookie with the access token
 function setAccessTokenCookie(res, token) {
-    res.cookie('access_token', token, { maxAge: 1000 * 60 * 10, httpOnly: false });
+    res.cookie('access_token', token, {
+        maxAge: 58 * 60 * 1000, //58 min,
+        httpOnly: true, // Make the cookie accessible only through the server-side
+        sameSite: 'None', // Allows cross-origin requests
+        secure: true,
+        path: "/",
+        domain: "nodessl.onrender.com"
+    });
 }
 
 function authorize(...roles) {
